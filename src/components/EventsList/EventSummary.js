979e6865939a41f0c94d4be0bb808b5cbb4db509
nonzeronucleus <gitkraken@ianplumb.org>
2018-11-25 21:18:45 +0000

    Added short text

--- src/components/EventsList/EventSummary.js
+++ src/components/EventsList/EventSummary.js
@@ -5,14 +5,16 @@ import { Link } from 'gatsby'
 import EventItem from '../EventItem';
 
 
-export default ({name,description, locationDescription, eventDate, location}) => {
+export default ({name, description, locationDescription, shortDescription, eventDate, location}) => {
   const locationURL = location
     ? `https://maps.google.com/?q=${location.lat},${location.lon}`
     : null;
 
+  const summary = shortDescription || description.childMarkdownRemark.excerpt;
+
   return (
     <Link to={`events/${encodeURI(name)}`}>
-      <EventItem {...{name, locationDescription, eventDate, locationURL}} descriptionBody={description.childMarkdownRemark.excerpt} />
+      <EventItem {...{name, locationDescription, eventDate, locationURL}} descriptionBody={summary} />
     </Link>
   )
 }
--- src/pages/events.js
+++ src/pages/events.js
@@ -27,6 +27,8 @@ export default () => (
                   excerpt
                 }
               }
+
+              shortDescription
             }
           }
         }
